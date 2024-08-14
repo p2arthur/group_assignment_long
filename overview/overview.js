f294d9af98c502435f964c4b3fc7f60804aa4a70
p2arthur <arthurrabelopro@gmail.com>
2024-08-13 23:13:11 -0700

    [upd:style] update overview page styles to make the layout work more fluid and apply best practices on css

--- overview/overview.js
+++ overview/overview.js
@@ -27,54 +27,73 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.log("weaterData", weatherData);
 
     titleElement.textContent = selectedLocation.name;
-    // <<<<<<< HEAD
-    //     weatherStats1.textContent = `🌡️ ${weatherData.current.apparent_temperature}°C`;
-    //     weatherStats2.textContent = `💨 ${weatherData.current.wind_speed_10m} KM/H`;
-    //     weatherStats3.textContent = `☁️ ${weatherData.current.cloud_cover}`;
-    //     weatherStats4.textContent = `☔ ${weatherData.current.rain}`;
-    //     weatherStats5.textContent = `⌚ ${weatherData.current.time}`;
-    //     weatherStats6.textContent = `💧 ${weatherData.current.relative_humidity_2m}`;
-    // =======
-    //     weatherStats1.textContent = `${weatherData.current?.apparent_temperature}°C`;
-    //     weatherStats2.textContent = `${weatherData.current.wind_speed_10m}KM/H`;
-    // <<<<<<< HEAD
-    //     locationVideo.setAttribute(
-    //       "src",
-    //       `${selectedLocation.video_url}?autoplay=1`
-    //     );
-    // =======
+
+    weatherStats1.textContent = `🌡️ ${weatherData.current.apparent_temperature}°C`;
+    weatherStats2.textContent = `💨 ${weatherData.current.wind_speed_10m} KM/H`;
+    weatherStats3.textContent = `☁️ ${weatherData.current.cloud_cover}`;
+    weatherStats4.textContent = `☔ ${weatherData.current.rain}`;
+    weatherStats5.textContent = `⌚ ${weatherData.current.time}`;
+    weatherStats6.textContent = `💧 ${weatherData.current.relative_humidity_2m}`;
+
+    weatherStats1.textContent = `${weatherData.current?.apparent_temperature}°C`;
+    weatherStats2.textContent = `${weatherData.current.wind_speed_10m}KM/H`;
+
+    locationVideo.setAttribute(
+      "src",
+      `${selectedLocation.video_url}?autoplay=1`
+    );
 
     locationVideo.setAttribute("src", selectedLocation.video_url);
 
-    // if  (weatherData.current.apparent_temperature < 10 && weatherData.current.rain <= 0){
-    //   cloth_1.textContent = '🧤';
-    //   cloth_2.textContent = '🧣';
-    //   cloth_3.textContent = '🧥';
-    //  } else if (weatherData.current.apparent_temperature < 0 && weatherData.current.rain > 0) {
-    //   cloth_1.textContent = '🧤';
-    //   cloth_2.textContent = '☂️';
-    //   cloth_3.textContent = '🧥';
-    //  } else if (weatherData.current.apparent_temperature > 0 && weatherData.current.rain > 0) {
-    //   cloth_1.textContent = '👖';
-    //   cloth_2.textContent = '☂️';
-    //   cloth_3.textContent = '🧥';
-    //  } else if (weatherData.current.apparent_temperature > 0 && weatherData.current.rain <= 0) {
-    //   cloth_1.textContent = '👖';
-    //   cloth_2.textContent = '👕';
-    //   cloth_3.textContent = '🧦';
-    //  }else if (weatherData.current.apparent_temperature > 20 && weatherData.current.rain < 0 && weatherData.current.cloud_cover == 0) {
-    //   cloth_1.textContent = '👕';
-    //   cloth_2.textContent = '🩳';
-    //   cloth_3.textContent = '🧢 🕶️';
-    //  } else if (weatherData.current.apparent_temperature > 15 && weatherData.current.rain < 0 && weatherData.current.cloud_cover > 0) {
-    //   cloth_1.textContent = '👕';
-    //   cloth_2.textContent = '🧥';
-    //   cloth_3.textContent = '👖';
-    //  }else {
-    //   cloth_1.textContent = '👖';
-    //   cloth_2.textContent = '👕';
-    //   cloth_3.textContent = '🧥';
-    //  }
+    if (
+      weatherData.current.apparent_temperature < 10 &&
+      weatherData.current.rain <= 0
+    ) {
+      cloth_1.textContent = "🧤";
+      cloth_2.textContent = "🧣";
+      cloth_3.textContent = "🧥";
+    } else if (
+      weatherData.current.apparent_temperature < 0 &&
+      weatherData.current.rain > 0
+    ) {
+      cloth_1.textContent = "🧤";
+      cloth_2.textContent = "☂️";
+      cloth_3.textContent = "🧥";
+    } else if (
+      weatherData.current.apparent_temperature > 0 &&
+      weatherData.current.rain > 0
+    ) {
+      cloth_1.textContent = "👖";
+      cloth_2.textContent = "☂️";
+      cloth_3.textContent = "🧥";
+    } else if (
+      weatherData.current.apparent_temperature > 0 &&
+      weatherData.current.rain <= 0
+    ) {
+      cloth_1.textContent = "👖";
+      cloth_2.textContent = "👕";
+      cloth_3.textContent = "🧦";
+    } else if (
+      weatherData.current.apparent_temperature > 20 &&
+      weatherData.current.rain < 0 &&
+      weatherData.current.cloud_cover == 0
+    ) {
+      cloth_1.textContent = "👕";
+      cloth_2.textContent = "🩳";
+      cloth_3.textContent = "🧢 🕶️";
+    } else if (
+      weatherData.current.apparent_temperature > 15 &&
+      weatherData.current.rain < 0 &&
+      weatherData.current.cloud_cover > 0
+    ) {
+      cloth_1.textContent = "👕";
+      cloth_2.textContent = "🧥";
+      cloth_3.textContent = "👖";
+    } else {
+      cloth_1.textContent = "👖";
+      cloth_2.textContent = "👕";
+      cloth_3.textContent = "🧥";
+    }
   } else {
     console.error("No location data found in sessionStorage");
   }
