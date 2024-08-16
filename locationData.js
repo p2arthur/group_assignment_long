2a9748180c638a49f54585a020e5952d89367653
vasylchenkoyehor <73816091+vasylchenkoyehor@users.noreply.github.com>
2024-08-15 20:05:44 -0700

    Update locationData.js
    Fixed names for the locations

--- locationData.js
+++ locationData.js
@@ -1,26 +1,26 @@
 export const locations = [
   {
-    name: "Kitslano Beach, Kitsilano, Vancouver, Canada",
+    name: "Kitslano Beach, Vancouver",
     location: { lat: 49.27, long: -123.15 },
     video_url: "https://www.youtube.com/embed/zRDmWuVlx8o",
   },
   {
-    name: "Fred Gingell Park",
+    name: "Fred Gingell Park, Delta",
     location: { lat: 49.0, long: -123.09 },
     video_url: "https://www.youtube.com/embed/JH7sESxSNY8",
   },
   {
-    name: "Whonnock Lake Park",
+    name: "Whonnock Lake Park, Maple Ridge",
     location: { lat: 49.2, long: -122.44 },
     video_url: "https://www.youtube.com/embed/1wkIQNevjy8",
   },
   {
-    name: "Pipers Lagoon Park",
+    name: "Pipers Lagoon Park, Nanaimo",
     location: { lat: 49.22, long: -123.94 },
     video_url: "https://www.youtube.com/embed/nIqFMitwWWs",
   },
   {
-    name: "Tasiilaq",
+    name: "Tasiilaq, Greenland",
     location: { lat: 65.61, long: -37.64 },
     video_url: "https://www.youtube.com/embed/mwG8G4F1wPU",
   },
